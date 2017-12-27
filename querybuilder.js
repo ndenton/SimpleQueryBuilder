591ce901da4fb2a7306129407d773469c53b9f04
Neil Denton <neild@dotnetdork.com>
2017-12-26 19:04:29 -0500

    Querybuilder Library

--- querybuilder.js
+++ querybuilder.js
@@ -1,46 +1,32 @@
-function query(qryObject) {
+function Query() {}
 
+Query.prototype.queryBuilder = (qryObject) => {
+
+    //parses out the query object
     this.selectValues = qryObject.selectVals
     this.filterValues = qryObject.filter || {}
     this.tableObject = qryObject.tables
     this.filter = qryObject.filtered || false
 
+    //returns an array of Columns from the selectValues section of the query object.
     this.scon = function() {
         return Object.entries(this.selectValues).map(key => {return `${key[1]}`}).join(', ')
     }
 
+    //returns an arry of Filter Values for the where statement from the filter section of 
+    //the query object.
     this.fcon = function() {
         let svalues = Object.entries(this.filterValues)
         return `${svalues.map(key => {return `${key[0]} = ${key[1]}`}).join(', ')}`
     }
 
+    //returns an array of tables to use from the tables section of the query object.
     this.tables = function() {
         return Object.entries(this.tableObject).map(key => {return `${key[0]}`})
     }
     
-    this.query = function() {
-        return `select ${this.scon()} from ${this.tables()} ${(this.filter ? 'where '  + this.fcon()  : '')}`
-    }
+    //returns the query string.
+    return `select ${this.scon()} from ${this.tables()} ${(this.filter ? 'where '  + this.fcon()  : '')}`
 }
 
-qry = {
-    tables: {
-        Users: {
-
-        }
-    }, 
-    selectVals: {
-        val1 : 'UserName',
-        val2 : 'UserFName',
-        val3 : 'UserLName'
-    },
-    filter: {
-        UserName: `'bjones'`,
-        UserFName: `'Bob'`,
-        UserLName: `'Jones'`
-    }, 
-    filtered: true
-}
-
-const i = new query(qry)
-console.log(i.query())
+module.exports = Query
